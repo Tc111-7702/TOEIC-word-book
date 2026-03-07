f1fb07351648fffe26818d89f0ae8f206bce8a46
Yuta Yamamoto <yutamoto235@gmail.com>
2026-03-07 18:37:00 +0900

    Fix: dynamic port and start script

--- server/server.js
+++ server/server.js
@@ -32,4 +32,7 @@ app.use(express.json());
 app.use("/", router);
 
 //サーバー起動
-app.listen(3000, () => console.log("server is running"));
+const PORT = process.env.PORT || 3000; 
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
