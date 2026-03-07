d4e79f97184f4b264cc602a9620d69e71c770d88
Yuta Yamamoto <yutamoto235@gmail.com>
2026-03-07 18:08:18 +0900

    Fix: absolute path for static files

--- server/server.js
+++ server/server.js
@@ -2,6 +2,19 @@ import express from "express";
 import cookieParser from "cookie-parser"; 
 import cors from "cors";
 import router from "./routes/router.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// ESモジュールでパスを扱うための設定
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// 静的ファイルの読み込み設定（srcフォルダを指定）
+app.use(express.static(path.join(__dirname, "src")));
+// 2. ルート（/）にアクセスしたときに index.html を強制的に表示する
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "src", "index.html"));
+});
 
 const app = express();
 //Cookieの解析
